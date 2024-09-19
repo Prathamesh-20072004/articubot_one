5948b06f4546d133653e5bcddc7d614145252dfd
Prathamesh-20072004 <madhavimadhavi947@gmail.com>
2024-09-19 17:05:44 +0530

    new node added

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -37,7 +37,16 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
-
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
 
     # Launch them all!
     return LaunchDescription([
